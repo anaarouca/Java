dad98e0fc2e1e3e6b6f2bdf9d4b84821d8cf4970
anaarouca <anajuliaaroucaalves@gmail.com>
2025-11-07 16:39:49 -0300

    Lista 24 - Java - Orientação Objeto - Herança

---
+++ primeiroProjeto/Lista24/src/Gato.java
@@ -0,0 +1,18 @@
+
+public class Gato {
+	
+	private String cor;
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public void subirNoMuro() {
+		System.out.println("O gato "+ getCor() + " está em cima do muro");	}
+	
+
+}
